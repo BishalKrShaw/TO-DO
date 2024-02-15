4478631c2e0bc04618adf320809466834ac649c6
BishalKrShaw <bishalshaw.mfis@gmail.com>
2024-02-15 16:27:42 +0530

    Checked Function Done

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 const addTaskBtn = document.querySelector("#addIcon");
 const displayTask = document.querySelector("#displayTaskListContainer ul");
+let inputTask = document.querySelector("#taskInputField");
 
 // Function to add Task
 function addTask() {
-    let inputTask = document.querySelector("#taskInputField");
-    const list = document.createElement("li");
-    list.innerHTML = `<input type="checkbox" class="todoCheck"><span class="todoTask">${inputTask.value}</span><span class="cross">&#10005;</span>`;
-    displayTask.appendChild(list);
-    inputTask.value = "";
+    if(inputTask.value === "") {
+        alert("You must write something");
+    }
+    else{
+        const list = document.createElement("li");
+        list.innerHTML = `<input type="checkbox" id="check" class="todoCheck"><label for="" class="todoTask">${inputTask.value}</label><span class="cross">&#10005;</span>`;
+        displayTask.appendChild(list);
+        inputTask.value = "";
+    }
 }
 
 addTaskBtn.addEventListener("click", ()=>{
     addTask();
 });
+
+// Feature to Mark Task as Checked
+displayTask.addEventListener("click", (e)=>{
+    if(e.target.type === "checkbox") {
+        const label = e.target.nextElementSibling;
+        label.classList.toggle("checkedList");
+    }
+}, false);
