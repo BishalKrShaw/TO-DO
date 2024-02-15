5ab6afaabb96ba86e0eaa2679ba678b9eb0b13ce
BishalKrShaw <bishalshaw.mfis@gmail.com>
2024-02-15 14:32:43 +0530

    Add Task Feature Done

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const checks = document.querySelectorAll(".todoCheck");
-const lists = document.querySelectorAll("li .todoTask");
+const addTaskBtn = document.querySelector("#addIcon");
+const displayTask = document.querySelector("#displayTaskListContainer ul");
 
-checks.forEach((check)=>{
-    check.addEventListener("click", ()=>{
-        lists.forEach((list)=>{
-            if(check.checked) {
-                list.classList.add("checkedList");
-            }
-            else{
-                list.classList.remove("checkedList");
-            }
-        });
-    });
+// Function to add Task
+function addTask() {
+    let inputTask = document.querySelector("#taskInputField");
+    const list = document.createElement("li");
+    list.innerHTML = `<input type="checkbox" class="todoCheck"><span class="todoTask">${inputTask.value}</span><span class="cross">&#10005;</span>`;
+    displayTask.appendChild(list);
+    inputTask.value = "";
+}
+
+addTaskBtn.addEventListener("click", ()=>{
+    addTask();
 });
